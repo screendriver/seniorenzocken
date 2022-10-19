facc0ba7d2dbc424ff856d33772e3b3281003958
Christian Rackerseder <git@echooff.de>
2022-10-19 14:13:50 +0200

    Specify server listening port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import sveltePreprocess from "svelte-preprocess";
 
 export default defineConfig({
+	server: {
+		port: 3000,
+		strictPort: true
+	},
 	plugins: [
 		svelte({
 			preprocess: [
