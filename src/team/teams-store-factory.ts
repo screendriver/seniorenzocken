cf4a2fe5d7d769af7acd6be27c0c73ec4e5575f6
Christian Rackerseder <git@echooff.de>
2022-10-20 20:24:00 +0200

    Create a Svelte store for storing teams

---
+++ src/team/teams-store-factory.ts
@@ -0,0 +1,49 @@
+import { writable, type Writable } from "svelte/store";
+import { z } from "zod";
+import { Maybe } from "true-myth";
+
+const teamSchema = z
+	.object({
+		teamName: z.string(),
+		teamNumber: z.number().nonnegative()
+	})
+	.strict();
+
+const allTeamsSchema = z.array(z.tuple([z.number(), teamSchema]));
+
+export type Team = z.infer<typeof teamSchema>;
+
+const storageKey = "teams";
+
+function mapTeamFromStorageToTeamMap(teamFromStorage: string): Map<number, Team> {
+	const emptyMap = new Map<number, Team>();
+
+	try {
+		const parseResult = allTeamsSchema.parse(JSON.parse(teamFromStorage));
+
+		return new Map(parseResult);
+	} catch {
+		return emptyMap;
+	}
+}
+
+export function createTeamsStore(storage: Storage): Writable<Map<number, Team>> {
+	const initialStoreValue = Maybe.of(storage.getItem(storageKey)).mapOr(
+		new Map<number, Team>(),
+		mapTeamFromStorageToTeamMap
+	);
+
+	const { set, subscribe, update } = writable(initialStoreValue);
+
+	subscribe((teams) => {
+		const teamsAsString = JSON.stringify(Array.from(teams.entries()));
+
+		storage.setItem(storageKey, teamsAsString);
+	});
+
+	return {
+		set,
+		subscribe,
+		update
+	};
+}
---
+++ test/team/teams-store-factory.test.ts
@@ -0,0 +1,168 @@
+import { assert, test, vi, type TestFunction } from "vitest";
+import { createTeamsStore, type Team } from "../../src/team/teams-store-factory";
+
+function createFakeStorage(overrides: Partial<Storage> = {}): Storage {
+	return {
+		getItem: overrides.getItem ?? vi.fn().mockReturnValue(null),
+		setItem: overrides.setItem ?? vi.fn()
+	} as unknown as Storage;
+}
+
+interface TestStoreValueOptions {
+	readonly storageValue: unknown;
+	readonly expectedStoreValue: ReadonlyMap<number, Team>;
+}
+
+function testStoreValue(testOptions: TestStoreValueOptions): TestFunction {
+	return () => {
+		const { storageValue, expectedStoreValue } = testOptions;
+		const getItem = vi.fn().mockReturnValue(storageValue);
+		const fakeStorage = createFakeStorage({ getItem });
+		const teamStore = createTeamsStore(fakeStorage);
+
+		let assertionCalled = false;
+
+		teamStore.subscribe((teamMap) => {
+			assert.deepStrictEqual(teamMap, expectedStoreValue);
+			assertionCalled = true;
+		});
+
+		assert.isTrue(assertionCalled);
+	};
+}
+
+test(
+	"createTeamsStore() sets store value to an empty Map when storage returns null",
+	testStoreValue({
+		storageValue: null,
+		expectedStoreValue: new Map()
+	})
+);
+
+test(
+	"createTeamsStore() sets store value to an empty Map when storage returns an empty string",
+	testStoreValue({
+		storageValue: "",
+		expectedStoreValue: new Map()
+	})
+);
+
+test(
+	"createTeamsStore() sets store value to an empty Map when storage returns an invalid JSON",
+	testStoreValue({
+		storageValue: "[",
+		expectedStoreValue: new Map()
+	})
+);
+
+test(
+	"createTeamsStore() sets store value to an empty Map when storage returns an empty Array",
+	testStoreValue({
+		storageValue: "[]",
+		expectedStoreValue: new Map()
+	})
+);
+
+test(
+	"createTeamsStore() sets store value to an empty Map when storage returns a filled Array with empty objects",
+	testStoreValue({
+		storageValue: "[{ }]",
+		expectedStoreValue: new Map()
+	})
+);
+
+test(
+	"createTeamsStore() sets store value to an empty Map when storage returns a filled Array filled with an empty Array",
+	testStoreValue({
+		storageValue: "[[ ]]",
+		expectedStoreValue: new Map()
+	})
+);
+
+test(
+	"createTeamsStore() sets store value to an empty Map when storage returns a filled Array with an empty objects",
+	testStoreValue({
+		storageValue: "[[1, { }]]",
+		expectedStoreValue: new Map()
+	})
+);
+
+test(
+	"createTeamsStore() sets store value to an empty Map when storage returns a filled Array with unknown properties",
+	testStoreValue({
+		storageValue: '[[1, { "foo": 42 }]]',
+		expectedStoreValue: new Map()
+	})
+);
+
+test(
+	"createTeamsStore() sets store value to an empty Map when storage returns a filled Array with additional unknown properties",
+	testStoreValue({
+		storageValue: '[[1, { "teamName": "", "teamNumber": 1, "foo": 42 }]]',
+		expectedStoreValue: new Map()
+	})
+);
+
+test(
+	"createTeamsStore() sets store value to an empty Map when storage returns an invalid 'teamName'",
+	testStoreValue({
+		storageValue: '[[1, { "teamName": {}, "teamNumber": 1 }]]',
+		expectedStoreValue: new Map()
+	})
+);
+
+test(
+	"createTeamsStore() sets store value to an empty Map when storage returns an invalid 'teamNumber'",
+	testStoreValue({
+		storageValue: '[[1, { "teamName": "", "teamNumber": "not-a-number" }]]',
+		expectedStoreValue: new Map()
+	})
+);
+
+test(
+	"createTeamsStore() sets store value correctly when storage returns one single team",
+	testStoreValue({
+		storageValue: '[[1, { "teamName": "", "teamNumber": 42 }]]',
+		expectedStoreValue: new Map([[1, { teamName: "", teamNumber: 42 }]])
+	})
+);
+
+test(
+	"createTeamsStore() sets store value correctly when storage returns more than one team",
+	testStoreValue({
+		storageValue: '[[1, { "teamName": "one", "teamNumber": 42 }], [2, { "teamName": "two", "teamNumber": 24 }]]',
+		expectedStoreValue: new Map([
+			[1, { teamName: "one", teamNumber: 42 }],
+			[2, { teamName: "two", teamNumber: 24 }]
+		])
+	})
+);
+
+test("createTeamsStore() sets item in storage when setting an item in the store", () => {
+	const setItem = vi.fn();
+	const fakeStorage = createFakeStorage({ setItem });
+	const teamsStore = createTeamsStore(fakeStorage);
+
+	teamsStore.set(new Map([[1, { teamName: "one", teamNumber: 42 }]]));
+
+	assert.strictEqual(setItem.mock.calls.length, 2);
+	assert.deepStrictEqual(setItem.mock.calls[0], ["teams", "[]"]);
+	assert.deepStrictEqual(setItem.mock.calls[1], ["teams", '[[1,{"teamName":"one","teamNumber":42}]]']);
+});
+
+test("createTeamsStore() sets item in storage when updating an item in the store", () => {
+	const setItem = vi.fn();
+	const fakeStorage = createFakeStorage({ setItem });
+	const teamsStore = createTeamsStore(fakeStorage);
+
+	teamsStore.update((teams) => {
+		return teams.set(1, {
+			teamName: "one",
+			teamNumber: 42
+		});
+	});
+
+	assert.strictEqual(setItem.mock.calls.length, 2);
+	assert.deepStrictEqual(setItem.mock.calls[0], ["teams", "[]"]);
+	assert.deepStrictEqual(setItem.mock.calls[1], ["teams", '[[1,{"teamName":"one","teamNumber":42}]]']);
+});
