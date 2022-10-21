a6cc9ff5f523f86c057cf81ee96b4140caaffb15
Christian Rackerseder <git@echooff.de>
2022-10-21 17:25:49 +0200

    Create Svelte store that stores if the game is started

---
+++ test/unit/game/game-store.test.ts
@@ -0,0 +1,17 @@
+import { get } from "svelte/store";
+import { assert, test } from "vitest";
+import { isGameStarted } from "../../../src/game/game-store";
+
+test("isGameStarted returns false by default", () => {
+	const started = get(isGameStarted);
+
+	assert.isFalse(started);
+});
+
+test("isGameStarted allows to set a boolean", () => {
+	isGameStarted.set(true);
+
+	const started = get(isGameStarted);
+
+	assert.isTrue(started);
+});
