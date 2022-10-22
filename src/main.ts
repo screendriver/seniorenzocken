86f4547f6c28c10b993ec1851e28e2c8b02d3815
Christian Rackerseder <git@echooff.de>
2022-10-22 13:04:57 +0200

    Use <body /> as application target

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import App from "./App.svelte";
 
-const appHtmlElement = document.getElementById("app");
+const htmlBodyElement = document.querySelector("body");
 
-if (appHtmlElement === null) {
+if (htmlBodyElement === null) {
 	throw new Error('Element with id "app" could not be found');
 }
 
-const app = new App({
-	target: appHtmlElement
 });
 
-export default app;
+new App({
+	target: htmlBodyElement,
+});
