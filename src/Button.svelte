c87bfe7c48da8cb36d41a70db2a649a24bd07115
Christian Rackerseder <git@echooff.de>
2022-10-23 10:53:50 +0200

    Create <Button /> component

---
+++ src/Button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let buttonType: "submit" | "button";
+	export let disabled = false;
+	export let value: string;
+</script>
+
+<input
+	type={buttonType}
+	{value}
+	{disabled}
+	on:click
+	class="bg-sky-500 text-white rounded-lg p-3 cursor-pointer hover:bg-sky-400 disabled:cursor-default disabled:hover:bg-sky-500 disabled:opacity-50"
+/>
