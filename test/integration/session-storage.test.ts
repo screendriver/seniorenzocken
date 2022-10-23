aca529602d8b59cc1ee3bbbb3527117f4b930e99
Christian Rackerseder <git@echooff.de>
2022-10-23 10:10:15 +0200

    Create integration test for saving if is game started into session storage

--- test/integration/session-storage.test.ts
+++ test/integration/session-storage.test.ts
@@ -14,3 +14,23 @@ test("saves entered team names into session storage", async ({ page }) => {
 		'[[1,{"teamName":"Test team","teamNumber":1}],[2,{"teamName":"","teamNumber":2}]]'
 	);
 });
+
+test("saves if is game started into session storage", async ({ page }) => {
+	await page.goto("/");
+	const inputElementTeam1 = page.getByPlaceholder("Team 1");
+	await inputElementTeam1.focus();
+	await inputElementTeam1.type("Test team 1");
+
+	const inputElementTeam2 = page.getByPlaceholder("Team 2");
+	await inputElementTeam2.focus();
+	await inputElementTeam2.type("Test team 2");
+
+	const submitButton = page.getByText("Spiel starten");
+	await submitButton.click();
+
+	const isGameStartedFromSessionStorage = await page.evaluate(() => {
+		return window.sessionStorage.getItem("is-game-started");
+	});
+
+	expect(isGameStartedFromSessionStorage).toStrictEqual("true");
+});
