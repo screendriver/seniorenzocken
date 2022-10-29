7ef5325aafe748a53511983c2a0a85ad06da7eb1
Christian Rackerseder <git@echooff.de>
2022-10-29 16:32:09 +0200

    Create end-to-end tests for teams form

---
+++ test/end-to-end/teams-form.test.ts
@@ -0,0 +1,81 @@
+import { test, expect } from "../playwright/test.js";
+
+test("disables the submit button when both teams are not filled initially", async ({ page, gamePage }) => {
+	await page.goto("/");
+
+	const isDisabled = await gamePage.isTeamsFormSubmitDisabled();
+
+	expect(isDisabled).toStrictEqual(true);
+});
+
+test("disables the submit button when something gets entered in first team", async ({ page, gamePage }) => {
+	await page.goto("/");
+	await gamePage.fillTeamOneInTeamsForm("Test team 1");
+
+	const isDisabled = await gamePage.isTeamsFormSubmitDisabled();
+
+	expect(isDisabled).toStrictEqual(true);
+});
+
+test("disables the submit button when something gets entered in second team", async ({ page, gamePage }) => {
+	await page.goto("/");
+	await gamePage.fillTeamTwoInTeamsForm("Test team 2");
+
+	const isDisabled = await gamePage.isTeamsFormSubmitDisabled();
+
+	expect(isDisabled).toStrictEqual(true);
+});
+
+test("disables the submit button again when team one was already filled but cleared afterwards", async ({
+	page,
+	gamePage
+}) => {
+	await page.goto("/");
+	await gamePage.fillTeamOneInTeamsForm("Test team 1");
+	await gamePage.fillTeamOneInTeamsForm("");
+
+	const isDisabled = await gamePage.isTeamsFormSubmitDisabled();
+
+	expect(isDisabled).toStrictEqual(true);
+});
+
+test("disables the submit button again when team two was already filled but cleared afterwards", async ({
+	page,
+	gamePage
+}) => {
+	await page.goto("/");
+	await gamePage.fillTeamTwoInTeamsForm("Test team 2");
+	await gamePage.fillTeamTwoInTeamsForm("");
+
+	const isDisabled = await gamePage.isTeamsFormSubmitDisabled();
+
+	expect(isDisabled).toStrictEqual(true);
+});
+
+test("disables the submit button again when team one and two was filled but team one is cleared afterwards", async ({
+	page,
+	gamePage
+}) => {
+	await page.goto("/");
+	await gamePage.fillTeamOneInTeamsForm("Test team 1");
+	await gamePage.fillTeamTwoInTeamsForm("Test team 1");
+	await gamePage.fillTeamOneInTeamsForm("");
+
+	const isDisabled = await gamePage.isTeamsFormSubmitDisabled();
+
+	expect(isDisabled).toStrictEqual(true);
+});
+
+test("disables the submit button again when team one and two was filled but team two is cleared afterwards", async ({
+	page,
+	gamePage
+}) => {
+	await page.goto("/");
+	await gamePage.fillTeamOneInTeamsForm("Test team 1");
+	await gamePage.fillTeamTwoInTeamsForm("Test team 1");
+	await gamePage.fillTeamTwoInTeamsForm("");
+
+	const isDisabled = await gamePage.isTeamsFormSubmitDisabled();
+
+	expect(isDisabled).toStrictEqual(true);
+});
