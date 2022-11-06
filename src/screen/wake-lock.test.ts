ddd53d2f6e59c3156ee5a461d4c0157749a3cc42
Christian Rackerseder <git@echooff.de>
2022-11-06 09:55:11 +0100

    Create function that requests a screen wake lock

--- src/screen/wake-lock.test.ts
+++ src/screen/wake-lock.test.ts
@@ -1,5 +1,12 @@
-import { assert, test } from "vitest";
-import { isWakeLockSupported } from "./wake-lock";
+import Result from "true-myth/result";
+import { assert, test, vi, type Mock } from "vitest";
+import { isWakeLockSupported, requestWakeLock } from "./wake-lock";
+
+function createNavigator(request: Mock = vi.fn()): Navigator {
+	return {
+		wakeLock: { request }
+	} as unknown as Navigator;
+}
 
 test('isWakeLockSupported() returns false when property "wakeLock" does not exist in navigator', () => {
 	const isSupported = isWakeLockSupported({} as unknown as Navigator);
@@ -8,7 +15,32 @@ test('isWakeLockSupported() returns false when property "wakeLock" does not exis
 });
 
 test('isWakeLockSupported() returns true when property "wakeLock" exists in navigator', () => {
-	const isSupported = isWakeLockSupported({ wakeLock: {} } as unknown as Navigator);
+	const isSupported = isWakeLockSupported(createNavigator());
 
 	assert.isTrue(isSupported);
+});
+
+test("requestWakeLock() returns an Result Err when requesting screen wake lock rejects with an Error", async () => {
+	const request = vi.fn().mockRejectedValue(new Error("Test error"));
+
+	const requestResult = await requestWakeLock(createNavigator(request));
+
+	assert.deepStrictEqual(requestResult, Result.err("Test error"));
+});
+
+test("requestWakeLock() returns an Result Err when requesting screen wake lock rejects with an a non error object", async () => {
+	const request = vi.fn().mockRejectedValue(42);
+
+	const requestResult = await requestWakeLock(createNavigator(request));
+
+	assert.deepStrictEqual(requestResult, Result.err("An unknown error occurred"));
+});
+
+test("requestWakeLock() returns an Result Ok when requesting screen wake lock succeeds", async () => {
+	const sentinel = {} as unknown as WakeLockSentinel;
+	const request = vi.fn().mockResolvedValue(sentinel);
+
+	const requestResult = await requestWakeLock(createNavigator(request));
+
+	assert.deepStrictEqual(requestResult, Result.ok(sentinel));
 });
