b0b062250618f3830a160d1f54467b00cfc33227
Christian Rackerseder <git@echooff.de>
2022-11-27 08:16:15 +0100

    Define playlist options as single object

--- src/audio/playlist.ts
+++ src/audio/playlist.ts
@@ -4,11 +4,14 @@ import type { Teams } from "../team/team-schema.js";
 
 const zeroPointsAudioFiles = ["0_1", "0_2", "0_3", "0_4", "0_5", "0_6"];
 
-export function createPlaylist(
-	teams: Teams,
-	includeStretched: boolean,
-	randomCollectionElement: typeof sample
-): readonly string[] {
+export interface CreatePlaylistOptions {
+	readonly teams: Teams;
+	readonly includeStretched: boolean;
+	readonly randomCollectionElement: typeof sample;
+}
+
+export function createPlaylist(options: CreatePlaylistOptions): readonly string[] {
+	const { teams, includeStretched, randomCollectionElement } = options;
 	const playlist: string[] = [];
 
 	let stretched = false;
