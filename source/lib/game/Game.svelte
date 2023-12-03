690b092429757fe1a42fb1e21018a4e8859f5a65
Christian Rackerseder <github@echooff.de>
2023-12-03 12:49:03 +0100

    Create <Game /> Svelte component

---
+++ source/lib/game/Game.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { ButtonGroup, Button } from "flowbite-svelte";
+	import GamePoint from "./GamePoint.svelte";
+	import canvasConfetti from "canvas-confetti";
+	import { gameStore } from "../game-store/game-store.js";
+
+	$: allTeamsAtZeroGamePoints = $gameStore.teams.every((team) => {
+		return team.currentGamePoints === 0;
+	});
+
+	$: if ($gameStore.shouldShowConfetti) {
+		void canvasConfetti({
+			particleCount: 100,
+			spread: 70,
+			origin: { y: 0.6 },
+		})?.then(() => {
+			gameStore.hideConfetti();
+		});
+	}
+
+	$: nextGameRoundEnabled = !allTeamsAtZeroGamePoints && !$gameStore.audioPlaying;
+	$: previousGameRoundEnabled = !$gameStore.gameRounds.empty() && !$gameStore.audioPlaying;
+</script>
+
+<form
+	on:submit|preventDefault
+	class="relative -top-20 mx-5 p-8 bg-slate-800 bg-opacity-90 rounded-lg shadow-md flex flex-col items-center gap-6 sm:max-w-lg sm:mx-auto"
+>
+	{#each $gameStore.teams as team (team.teamNumber)}
+		{@const enabled = (allTeamsAtZeroGamePoints || team.currentGamePoints > 0) && !$gameStore.audioPlaying}
+
+		<GamePoint {team} {enabled} />
+	{/each}
+
+	<ButtonGroup>
+		<Button disabled={!previousGameRoundEnabled} on:click={gameStore.previousGameRound} color="yellow"
+			>Runde Zurück</Button
+		>
+
+		<Button disabled={!nextGameRoundEnabled} on:click={gameStore.nextGameRound} color="primary">
+			Nächste Runde
+		</Button>
+	</ButtonGroup>
+</form>
