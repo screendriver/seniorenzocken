8793ade08059f1ebb83073cb13acda25c5849193
Christian Rackerseder <github@echooff.de>
2024-03-03 11:42:49 +0100

    Configure Tailwind CSS for daisyUI

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,25 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-	content: ["./source/app.html", "./source/**/*.svelte", "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
-	plugins: [require("flowbite/plugin")],
+	content: ["./source/app.html", "./source/**/*.svelte"],
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: ["dark"],
+	},
 	theme: {
 		extend: {
-			colors: {
-				// flowbite-svelte
-				primary: {
-					50: "#ecfeff",
-					100: "#cffafe",
-					200: "#a5f3fc",
-					300: "#67e8f9",
-					400: "#22d3ee",
-					500: "#06b6d4",
-					600: "#0891b2",
-					700: "#0e7490",
-					800: "#155e75",
-					900: "#164e63",
-				},
-			},
 			animation: {
 				"blinking-text": "blinkingText 0.8s infinite",
 			},
