2b1731b51a55721b94e0436f9035dc962343d4da
Christian Rackerseder <github@echooff.de>
2024-05-29 23:46:42 +0200

    Revert "Remove statistics tracking"
    This reverts commit e236200a2bc66ad1593b0f32295d0cd1b480ec71.

--- source/routes/+page.svelte
+++ source/routes/+page.svelte
@@ -41,6 +41,18 @@
 	});
 </script>
 
+<svelte:head>
+	{#if import.meta.env.MODE === "production"}
+		<script
+			async={true}
+			defer={true}
+			data-website-id="19ad1338-6801-4b9b-b08c-f38e9ed95c86"
+			data-do-not-track="true"
+			src="https://statistics.echooff.de/tasty.js"
+		></script>
+	{/if}
+</svelte:head>
+
 <WakeLock />
 
 <SettingsMenu />
