881c9b21fec6c7989a69f7af4da975fbe93dea0d
Christian Rackerseder <git@echooff.de>
2024-06-22 15:02:55 +0200

    Enable Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	theme: {
+		extend: {
+			animation: {
+				"blinking-text": "blinkingText 0.8s infinite",
+			},
+			keyframes: {
+				blinkingText: {
+					"0%": {
+						opacity: 0,
+					},
+
+					"49%": {
+						opacity: 0,
+					},
+
+					"50%": {
+						opacity: 1,
+					},
+				},
+			},
+		},
+	},
+	plugins: [],
+};
