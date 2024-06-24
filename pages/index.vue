fac0888ff852cffe1d3399c3de341957bafc296d
Christian Rackerseder <git@echooff.de>
2024-06-24 19:37:48 +0200

    Redirect permanently from root to /teams route

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
-<template>
-	<h1>Index page</h1>
-</template>
+<script setup lang="ts">
+definePageMeta({
+	middleware: [
+		() => {
+			return navigateTo("/teams", { redirectCode: 301 });
+		},
+	],
+});
+</script>
