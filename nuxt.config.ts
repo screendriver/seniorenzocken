317ed63789e18cb129225ed873f2558bc86133ac
Christian Rackerseder <git@echooff.de>
2024-06-26 13:44:31 +0200

    Add meta tags to head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,14 @@ export default defineNuxtConfig({
 				lang: "de",
 				"data-theme": "business",
 			},
-			title: "Seniorenzocken",
+			title: "Senioren zocken",
+			meta: [
+				{ "http-equiv": "Accept-CH", content: "DPR, Width" },
+				{ name: "description", content: "Punkte sammeln für das Kartenspiel Watten" },
+				{ name: "keywords", content: "Watten,Watteln,Wattlung,Kartenspiel,Punkte,Bayern" },
+				{ name: "theme-color", content: "#075985" },
+				{ name: "robots", content: "noimageindex" },
+			],
 		},
 	},
 });
