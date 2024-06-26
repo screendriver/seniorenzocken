a0e4670f4632f08a1fe33e46c8bec1fc9a05c5ab
Christian Rackerseder <git@echooff.de>
2024-06-26 17:49:51 +0200

    Replace beforeEach() with higher order function

--- stores/__tests__/game-store.test.ts
+++ stores/__tests__/game-store.test.ts
@@ -1,4 +1,4 @@
-import { test, expect, beforeEach } from "vitest";
+import { test, expect, type TestFunction } from "vitest";
 import { setActivePinia, createPinia } from "pinia";
 import { Factory } from "fishery";
 import { useGameStore } from "../game-store";
@@ -12,20 +12,30 @@ const teamFactory = Factory.define<Team>(() => {
 	};
 });
 
-beforeEach(() => {
+function withActivePinia(test: () => void): TestFunction {
 	setActivePinia(createPinia());
-});
 
-test("game store has an initial team1 set", () => {
-	const gameStore = useGameStore();
-	const expected = teamFactory.build({ teamNumber: 1 });
+	return () => {
+		test();
+	};
+}
 
-	expect(gameStore.team1).toEqual(expected);
-});
+test(
+	"game store has an initial team1 set",
+	withActivePinia(() => {
+		const gameStore = useGameStore();
+		const expected = teamFactory.build({ teamNumber: 1 });
 
-test("game store has an initial team2 set", () => {
-	const gameStore = useGameStore();
-	const expected = teamFactory.build({ teamNumber: 2 });
+		expect(gameStore.team1).toEqual(expected);
+	}),
+);
 
-	expect(gameStore.team2).toEqual(expected);
-});
+test(
+	"game store has an initial team2 set",
+	withActivePinia(() => {
+		const gameStore = useGameStore();
+		const expected = teamFactory.build({ teamNumber: 2 });
+
+		expect(gameStore.team2).toEqual(expected);
+	}),
+);
