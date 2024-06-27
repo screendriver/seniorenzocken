979bf3e27be639787070e573579077bd38a5d331
Christian Rackerseder <git@echooff.de>
2024-06-27 18:37:08 +0200

    Remove dummy Vue component

--- components/HelloWorld.nuxt.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "vitest";
-import { mountSuspended } from "@nuxt/test-utils/runtime";
-import HelloWorld from "./HelloWorld.vue";
-
-test("<HelloWorld />", async () => {
-	const wrapper = await mountSuspended(HelloWorld);
-
-	expect(wrapper.text()).toContain("Hello world");
-});
