098d95ed4e9e8bbb4591dfff5bd774f1b8140855
Christian Rackerseder <git@echooff.de>
2024-07-03 11:58:55 +0200

    Create function that checks if a team has reached maximum game points

--- app/utils/game-points.ts
+++ app/utils/game-points.ts
@@ -1,3 +1,7 @@
 export const availableGamePoints = [0, 2, 3, 4] as const;
 
 export type GamePoint = (typeof availableGamePoints)[number];
+
+export function hasReachedMaximumGamePoints(team1GamePoint: Ref<GamePoint>, team2GamePoint: Ref<GamePoint>): boolean {
+	return team1GamePoint.value == 4 || team2GamePoint.value == 4;
+}
