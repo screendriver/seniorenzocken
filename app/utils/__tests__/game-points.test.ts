b9bf63ae171de323dec982fba128bf7f100a4892
Christian Rackerseder <git@echooff.de>
2024-07-03 11:37:08 +0200

    Move game-points module into utils/ directory

--- app/utils/__tests__/game-points.test.ts
+++ app/utils/__tests__/game-points.test.ts
@@ -1,5 +1,4 @@
 import { test, expect } from "vitest";
-import { availableGamePoints } from "../game-points";
 
 test("availableGamePoints defines 4 available game points", () => {
 	expect(availableGamePoints).toStrictEqual([0, 2, 3, 4]);
