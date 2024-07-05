493bbfe5067e6393c5a62c3e175c6145e932c93d
Christian Rackerseder <git@echooff.de>
2024-07-05 22:17:03 +0200

    Return media records instead of using a reference

--- app/composables/use-pocketbase.ts
+++ app/composables/use-pocketbase.ts
@@ -2,14 +2,13 @@ import PocketBase from "pocketbase";
 import * as v from "valibot";
 
 export function useMediaRecords() {
-	const mediaRecords = ref<MediaRecords>([]);
 	const runtimeConfig = useRuntimeConfig();
 
 	async function fetchFullList() {
 		const pb = new PocketBase(runtimeConfig.public.pocketbaseBaseUrl);
 
 		const records = await pb.collection("media").getFullList();
-		mediaRecords.value = v.parse(mediaRecordsSchema, records);
+		return v.parse(mediaRecordsSchema, records);
 	}
 
 	return { fetchFullList };
