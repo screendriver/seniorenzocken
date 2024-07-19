3231ab3282fb9d4caba84d75eb8695f66be81364
Christian Rackerseder <git@echooff.de>
2024-07-19 16:13:45 +0200

    Create function to determine the winner team

---
+++ app/utils/teams.ts
@@ -0,0 +1,18 @@
+import type { Result } from "true-myth";
+import { ok as resultOk, err as resultError } from "true-myth/result";
+
+function teamsHaveSameGamePoints(team1: Ref<Team>, team2: Ref<Team>): boolean {
+	return team1.value.gamePoints === team2.value.gamePoints;
+}
+
+export function determineWinnerTeam(team1: Ref<Team>, team2: Ref<Team>): Result<Ref<Team>, string> {
+	if (teamsHaveSameGamePoints(team1, team2)) {
+		return resultError("Both teams have the same game points");
+	}
+
+	if (team1.value.gamePoints > team2.value.gamePoints) {
+		return resultOk(team1);
+	}
+
+	return resultOk(team2);
+}
