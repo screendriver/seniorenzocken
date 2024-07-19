41717ed4a5fa247adcffb062e98a7982a0df99dc
Christian Rackerseder <git@echooff.de>
2024-07-19 13:29:26 +0200

    Change types in teams type aliases

--- app/utils/team.ts
+++ app/utils/team.ts
@@ -5,11 +5,11 @@ export type TeamNumber = 1 | 2;
 export type Team = {
 	readonly teamNumber: TeamNumber;
 	teamName: string;
-	gamePoints: number;
+	gamePoints: GamePoints;
 	isStretched: boolean;
 };
 
-export type Teams = ReadonlyTuple<Team, 2>;
+export type Teams = ReadonlyTuple<Ref<Team>, 2>;
 
 export function createInitialTeam(teamNumber: TeamNumber): Team {
 	return {
