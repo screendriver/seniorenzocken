7c1aebeb1c4c4e029a06a06c41e0394cddaf4b3b
Christian Rackerseder <git@echooff.de>
2024-07-19 22:02:31 +0200

    Create <GameOver /> Vue component

---
+++ app/components/GameOver.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const gameStore = useGameStore();
+const { team1, team2, isAudioPlaying, shouldPlayAudio } = storeToRefs(gameStore);
+
+const isReplayAudioButtonDisabled = computed(() => {
+	return isAudioPlaying.value || !shouldPlayAudio.value;
+});
+
+const wonText = determineWinnerTeam(team1, team2).mapOr("Gewonnen hat: ???", (team) => {
+	return `Gewonnen hat: Team "${team.value.teamName}"`;
+});
+</script>
+
+<template>
+	<section class="col-start-2 col-end-6 my-8 gap-2 grid grid-cols-subgrid grid-rows-3">
+		<h1 class="col-span-full place-self-center">{{ wonText }}</h1>
+
+		<button type="button" class="btn col-start-2 col-end-4" @click="gameStore.startNewGame">Neues Spiel</button>
+		<button
+			:disabled="isReplayAudioButtonDisabled"
+			type="button"
+			class="btn col-start-2 col-end-4"
+			@click="isAudioPlaying = true"
+		>
+			Punktestand vorlesen
+		</button>
+	</section>
+</template>
