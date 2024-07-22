a3028a3183f783d324814987bf03e2e65173b744
Christian Rackerseder <git@echooff.de>
2024-07-22 06:48:21 +0200

    Enable collecting statistics

--- app/app.vue
+++ app/app.vue
@@ -5,6 +5,22 @@ const runtimeConfig = useRuntimeConfig();
 const pocketBase = new PocketBase(runtimeConfig.public.pocketBaseBaseUrl);
 
 provide(pocketBaseInjectionKey, pocketBase);
+
+onMounted(() => {
+	if (import.meta.env.PROD) {
+		useHead({
+			script: [
+				{
+					async: true,
+					defer: true,
+					"data-website-id": "19ad1338-6801-4b9b-b08c-f38e9ed95c86",
+					"data-do-not-track": true,
+					src: "https://statistics.echooff.de/tasty.js",
+				},
+			],
+		});
+	}
+});
 </script>
 
 <template>
