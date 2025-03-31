c27389796c267eab64170ebbfb6917140584bcac
Christian Rackerseder <git@echooff.de>
2025-03-31 09:26:24 +0200

    Change import path

--- source/main.ts
+++ source/main.ts
@@ -2,7 +2,7 @@ import "./assets/css/tailwind.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { createHead } from "@unhead/vue";
+import { createHead } from "@unhead/vue/client";
 
 import App from "./App.vue";
 import { router } from "./router";
