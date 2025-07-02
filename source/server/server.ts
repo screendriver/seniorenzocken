8b9aa43bc8901ba5a41932e12a81a0bfe7bf7db6
Christian Rackerseder <git@echooff.de>
2025-07-02 12:16:00 +0200

    Create /graphql route listener

--- source/server/server.ts
+++ source/server/server.ts
@@ -1,8 +1,16 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
+import { createYoga } from "graphql-yoga";
+import { schema } from "./schema.ts";
 
 const honoServer = new Hono();
+
+const yoga = createYoga({ schema });
+
+honoServer.use("/graphql", async (context) => {
+	return yoga.handle(context.req.raw, {});
+});
 
 honoServer.use("*", serveStatic({ root: "./browser-application" }));
 
