c192b318ae9ca91a171fd3d4529ede47ff728153
Christian Rackerseder <git@echooff.de>
2025-07-14 16:33:36 +0200

    Create audio repository

---
+++ source/server/audio/repository.ts
@@ -0,0 +1,60 @@
+import { eq, or, sql } from "drizzle-orm";
+import type { Database } from "../database/database.ts";
+import { gamePointAudios, type GamePointAudio } from "../database/schema.ts";
+import type { MatchTotalGamePoints } from "../game-points/game-points.ts";
+
+type ReadAudio = Pick<GamePointAudio, "gamePointAudioId" | "name" | "gamePoints">;
+
+type ReadAllAudiosOptions = {
+	readonly team1MatchTotalGamePoints: MatchTotalGamePoints;
+	readonly team2MatchTotalGamePoints: MatchTotalGamePoints;
+};
+
+export type AudioRepository = {
+	readAllAudios(options: ReadAllAudiosOptions): Promise<readonly ReadAudio[]>;
+};
+
+type AudioRepositoryOptions = {
+	readonly database: Database;
+};
+
+export function createAudioRepository(options: AudioRepositoryOptions): AudioRepository {
+	const { database } = options;
+
+	return {
+		async readAllAudios(options) {
+			const { team1MatchTotalGamePoints, team2MatchTotalGamePoints } = options;
+
+			return database
+				.select({
+					gamePointAudioId: gamePointAudios.gamePointAudioId,
+					name: gamePointAudios.name,
+					gamePoints: gamePointAudios.gamePoints,
+				})
+				.from(gamePointAudios)
+				.where(
+					or(
+						eq(gamePointAudios.name, "attention.m4a"),
+						eq(gamePointAudios.gamePoints, team1MatchTotalGamePoints),
+						eq(gamePointAudios.name, "to.m4a"),
+						eq(gamePointAudios.gamePoints, team2MatchTotalGamePoints),
+						eq(gamePointAudios.name, "stretched.m4a"),
+						eq(gamePointAudios.name, "won.m4a"),
+					),
+				)
+				.orderBy(
+					sql`
+						CASE
+							WHEN ${gamePointAudios.name} = 'attention.m4a' THEN 1
+							WHEN ${gamePointAudios.gamePoints} = ${team1MatchTotalGamePoints} THEN 2
+							WHEN ${gamePointAudios.name} = 'to.m4a' THEN 3
+							WHEN ${gamePointAudios.gamePoints} = ${team2MatchTotalGamePoints} THEN 4
+							WHEN ${gamePointAudios.name} = 'stretched.m4a' THEN 5
+							WHEN ${gamePointAudios.name} = 'won.m4a' THEN 6
+							ELSE 7
+						END
+					`,
+				);
+		},
+	};
+}
