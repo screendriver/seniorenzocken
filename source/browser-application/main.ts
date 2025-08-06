eed0708d6795fba457c5ff99126945223573b503
Christian Rackerseder <git@echooff.de>
2025-08-06 13:06:26 +0200

    Initialize Vue Sentry integration

--- source/browser-application/main.ts
+++ source/browser-application/main.ts
@@ -3,6 +3,8 @@ import "./assets/css/tailwind.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { createHead } from "@unhead/vue/client";
+import * as Sentry from "@sentry/vue";
+import { createSentryPiniaPlugin } from "@sentry/vue";
 
 import App from "./App.vue";
 import { router } from "./router.js";
@@ -10,6 +12,17 @@ import { router } from "./router.js";
 const app = createApp(App);
 const pinia = createPinia();
 const head = createHead();
+
+if (import.meta.env.PROD) {
+	Sentry.init({
+		app,
+		dsn: "https://a63e7259b4d94e0db547e9934a617ea8@bugsink.82r.de/1",
+		integrations: [],
+		tracesSampleRate: 0,
+	});
+}
+
+pinia.use(createSentryPiniaPlugin());
 
 app.use(pinia);
 app.use(router);
