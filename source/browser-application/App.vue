86eae2df5e416afee375f6f8c2948829aa222565
Christian Rackerseder <git@echooff.de>
2025-08-12 08:43:10 +0200

    Remove asynchronous loading of statistics script

--- source/browser-application/App.vue
+++ source/browser-application/App.vue
@@ -15,7 +15,6 @@ onMounted(() => {
 		useHead({
 			script: [
 				{
-					async: true,
 					defer: true,
 					"data-website-id": "16d1825d-3f6c-46fb-9243-1d281224605e",
 					src: "https://statistics.82r.de/tasty.js"
