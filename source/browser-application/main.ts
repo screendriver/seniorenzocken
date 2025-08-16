3fd20c51f72e2181d77c1b920e7819bb9ac0ee55
Christian Rackerseder <git@echooff.de>
2025-08-16 09:46:13 +0200

    Add browser tracing integration

--- source/browser-application/main.ts
+++ source/browser-application/main.ts
@@ -12,7 +12,7 @@ if (import.meta.env.PROD) {
 	Sentry.init({
 		app,
 		dsn: "https://a63e7259b4d94e0db547e9934a617ea8@bugsink.82r.de/1",
-		integrations: [],
+		integrations: [Sentry.browserTracingIntegration({ router })],
 		sendDefaultPii: true,
 		tracesSampleRate: 0
 	});
