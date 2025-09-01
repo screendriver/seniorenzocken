df9e8e580167d590a2b81eae513c084901c2ab80
Christian Rackerseder <git@echooff.de>
2025-09-01 12:30:51 +0200

    Create createSession() in session repository

--- source/server/session/session-repository.ts
+++ source/server/session/session-repository.ts
@@ -1,3 +1,4 @@
+import type { randomUUID } from "node:crypto";
 import { Task, tryOrElse } from "true-myth/task";
 import { eq } from "drizzle-orm";
 import { safeParse, summarize } from "valibot";
@@ -6,17 +7,24 @@ import { sessions as sessionsDatabaseSchema } from "../database/schema.js";
 import type { Database } from "../database/database.js";
 import { sessionSchema, type Session } from "./session-schema.js";
 
+type CreateSessionOptions = {
+	readonly ipAddress?: string;
+	readonly userAgent?: string;
+};
+
 export type SessionRepository = {
 	readonly getSession: (sessionToken: string) => Task<Session, Error>;
+	readonly createSession: (options: CreateSessionOptions) => Task<Session, Error>;
 	readonly deleteSession: (sessionToken: string) => Task<Unit, Error>;
 };
 
 type SessionRepositoryDependencies = {
 	readonly database: Database;
+	readonly randomUUID: typeof randomUUID;
 };
 
 export function createSessionRepository(dependencies: SessionRepositoryDependencies): SessionRepository {
-	const { database } = dependencies;
+	const { database, randomUUID } = dependencies;
 
 	return {
 		getSession(sessionToken) {
@@ -43,6 +51,40 @@ export function createSessionRepository(dependencies: SessionRepositoryDependenc
 				}
 
 				return Task.reject(new Error("Could not parse session from database", { cause: summarize(issues) }));
+			});
+		},
+
+		createSession(options) {
+			return tryOrElse(
+				(error: unknown) => {
+					return new Error("Could not create session", { cause: error });
+				},
+				async () => {
+					return database
+						.insert(sessionsDatabaseSchema)
+						.values({
+							token: randomUUID(),
+							ipAddress: options.ipAddress,
+							userAgent: options.userAgent
+						})
+						.returning({
+							token: sessionsDatabaseSchema.token,
+							ipAddress: sessionsDatabaseSchema.ipAddress,
+							userAgent: sessionsDatabaseSchema.userAgent
+						});
+				}
+			).andThen((databaseRecords) => {
+				const { success, output, issues } = safeParse(sessionSchema, databaseRecords[0]);
+
+				if (success) {
+					return Task.resolve({
+						token: output.token,
+						ipAddress: output.ipAddress,
+						userAgent: output.userAgent
+					});
+				}
+
+				return Task.reject(new Error("Could not create session", { cause: summarize(issues) }));
 			});
 		},
 
