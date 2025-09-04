f396ba73a0424d5127ce9a10de96ec53719cef83
Christian Rackerseder <git@echooff.de>
2025-09-04 09:25:43 +0200

    Render players selection

--- source/browser-application/tanstack-query.d.ts
+++ source/browser-application/tanstack-query.d.ts
@@ -1,6 +1,6 @@
 import "@tanstack/vue-query";
 
-type QueryKey = [...(readonly unknown[])];
+type QueryKey = ["players", ...(readonly unknown[])];
 
 declare module "@tanstack/vue-query" {
 	interface Register {
---
+++ source/browser-application/teams-selection/SelectPlayer.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { Players } from "../../server-shared/player.js";
+
+const { playerNumber, players } = defineProps<{
+	readonly playerNumber: number;
+	readonly players: Players;
+}>();
+</script>
+
+<template>
+	<select class="select w-full">
+		<option disabled selected>Spieler {{ playerNumber }}</option>
+
+		<option v-for="player in players" :key="player.playerId">{{ player.nickname }}</option>
+	</select>
+</template>
