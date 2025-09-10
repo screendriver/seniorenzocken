6b85667597ceca63879d4918f21f8f0eff0346a0
Christian Rackerseder <git@echooff.de>
2025-09-10 17:08:01 +0200

    Invalidate session queries fater sign-in

--- source/browser-application/tanstack-query.d.ts
+++ source/browser-application/tanstack-query.d.ts
@@ -1,6 +1,6 @@
 import "@tanstack/vue-query";
 
-type QueryKey = ["players" | "currentGameRound", ...(readonly unknown[])];
+type QueryKey = ["session" | "players" | "currentGameRound", ...(readonly unknown[])];
 
 declare module "@tanstack/vue-query" {
 	interface Register {
