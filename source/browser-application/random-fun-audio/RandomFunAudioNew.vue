86e89707fdf3b3cc8cfc0725f46008b8086b5c38
Christian Rackerseder <git@echooff.de>
2025-09-29 11:50:32 +0200

    Use TanStack query to fetch random fun audio

--- source/browser-application/random-fun-audio/RandomFunAudioNew.vue
+++ source/browser-application/random-fun-audio/RandomFunAudioNew.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { inject, ref, useTemplateRef, watch } from "vue";
+import { computed, inject, ref, useTemplateRef, watch } from "vue";
 import { onKeyStroke, useMouse, useMousePressed, useTimeoutFn } from "@vueuse/core";
-import { isNull } from "@sindresorhus/is";
+import { isNull, isUndefined } from "@sindresorhus/is";
 import { assertDefined } from "ts-extras";
+import { useQuery } from "@tanstack/vue-query";
 import { trpcClientInjectionKey } from "../trpc/client.js";
 
 const trpcClient = inject(trpcClientInjectionKey);
@@ -10,17 +11,11 @@ const trpcClient = inject(trpcClientInjectionKey);
 assertDefined(trpcClient);
 
 const emptyAudioAlreadyPlayed = ref(false);
+const playRandomFunAudio = ref(false);
+const audioTickCounter = ref(0);
 const audioElementReference = useTemplateRef("audio");
 const { x, y } = useMouse();
 const { pressed } = useMousePressed();
-
-function playEmptyAudio(): void {
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-call -- this rule should not complain here
-	void audioElementReference.value?.play();
-	emptyAudioAlreadyPlayed.value = true;
-}
-
-defineExpose({ playEmptyAudio });
 
 function getRandomDelay(): number {
 	const oneMinuteInMilliseconds = 60_000;
@@ -31,24 +26,24 @@ function getRandomDelay(): number {
 }
 
 const { start: startTimer, stop: stopTimer } = useTimeoutFn(
-	async () => {
-		if (isNull(audioElementReference.value) || !emptyAudioAlreadyPlayed.value) {
-			return;
-		}
-
-		const randomFunAudioUrl = await trpcClient.audio.getRandomFunAudio.query();
-
-		// eslint-disable-next-line require-atomic-updates -- done on purpose
-		audioElementReference.value.src = randomFunAudioUrl;
-
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-call -- this rule should not complain here
-		await audioElementReference.value.play();
-
-		startTimer();
+	() => {
+		playRandomFunAudio.value = true;
 	},
 	getRandomDelay,
 	{ immediate: false }
 );
+
+const randomFunAudioQueryEnabled = computed(() => {
+	return emptyAudioAlreadyPlayed.value && playRandomFunAudio.value;
+});
+
+const { data: randomFunAudioUrl } = useQuery({
+	queryKey: ["randomFunAudio", audioTickCounter],
+	async queryFn() {
+		return trpcClient.audio.getRandomFunAudio.query();
+	},
+	enabled: randomFunAudioQueryEnabled
+});
 
 function restartTimer(): void {
 	if (isNull(audioElementReference.value) || !emptyAudioAlreadyPlayed.value) {
@@ -62,6 +57,36 @@ function restartTimer(): void {
 onKeyStroke(restartTimer);
 
 watch([x, y, pressed], restartTimer);
+
+watch(randomFunAudioUrl, async (randomFunAudioUrlValue) => {
+	if (isUndefined(randomFunAudioUrlValue) || isNull(audioElementReference.value)) {
+		return;
+	}
+
+	audioElementReference.value.src = randomFunAudioUrlValue;
+
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call -- this rule should not complain here
+	await audioElementReference.value.play();
+
+	playRandomFunAudio.value = false;
+	audioTickCounter.value += 1;
+	startTimer();
+});
+
+async function playEmptyAudio(): Promise<void> {
+	if (isNull(audioElementReference.value)) {
+		return;
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call -- this rule should not complain here
+	await audioElementReference.value.play();
+
+	emptyAudioAlreadyPlayed.value = true;
+
+	startTimer();
+}
+
+defineExpose({ playEmptyAudio });
 </script>
 
 <template>
--- source/browser-application/tanstack-query.d.ts
+++ source/browser-application/tanstack-query.d.ts
@@ -1,6 +1,6 @@
 import "@tanstack/vue-query";
 
-type QueryKey = ["session" | "players" | "currentGameRound", ...(readonly unknown[])];
+type QueryKey = ["session" | "players" | "currentGameRound" | "randomFunAudio", ...(readonly unknown[])];
 
 declare module "@tanstack/vue-query" {
 	interface Register {
