64a91655ffd355aaabfd7730e1d4936c4608659c
Christian Rackerseder <git@echooff.de>
2026-08-02 11:55:45 +0200

    Remove unused Infisical secret integration

--- source/server/secrets/infisical/secret-schema.ts
+++
@@ -1,7 +0,0 @@
-import { object, pipe, string, nonEmpty, type InferOutput } from "valibot";
-
-export const secretSchema = object({
-	secretValue: pipe(string(), nonEmpty())
-});
-
-export type Secret = InferOutput<typeof secretSchema>;
